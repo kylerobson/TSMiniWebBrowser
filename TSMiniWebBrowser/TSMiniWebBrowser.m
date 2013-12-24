5fcc83537a2d1e43a1850b592c47976b8c421a30
= <=>
2013-12-24 00:13:29 -0600

    no message

--- TSMiniWebBrowser/TSMiniWebBrowser.m
+++ TSMiniWebBrowser/TSMiniWebBrowser.m
@@ -24,6 +24,13 @@
 //  THE SOFTWARE.
 //
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+#define IOS7PLUS (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
+
 #import "TSMiniWebBrowser.h"
 
 @implementation TSMiniWebBrowser
